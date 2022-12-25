30e33d80f93c56b8072156a9010d97280e0f9b4a
Annie <overheads165@gmail.com>
2022-12-26 00:13:49 +0100

    popover done

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,13 @@ const button = document.querySelector("#button");
 const popover = document.querySelector("#popover");
 
 button.addEventListener("click", function () {
-  const { x, y, width, height } = button.getBoundingClientRect();
-  const { width: popoverWidth, height: popoverHeight } =
+    popover.classList.toggle("d-none");
+    
+    const { x, y, width, height } = button.getBoundingClientRect();
+    const { width: popoverWidth, height: popoverHeight } =
     popover.getBoundingClientRect();
-
-  popover.style.top = y - popoverHeight - 10 + "px";
-  popover.style.left = x - (popoverWidth - width) / 2 + "px";
+    
+    popover.style.top = y - popoverHeight - 10 + "px";
+    popover.style.left = x - (popoverWidth - width) / 2 + "px";
+    
 });
